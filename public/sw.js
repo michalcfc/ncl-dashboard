a75626478d6c9a58f7175181ad33c1f9714c5c51
michal_cfc <michal.jachimowski@gmail.com>
2022-05-24 11:47:15 +0200

    removed netlify connection

--- public/sw.js
+++ public/sw.js
@@ -17,26 +17,26 @@ self.addEventListener("install", (event) => {
 
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function(err) {
-
-            });
-        }
-      })
-  );
+  // event.respondWith(
+  //   caches.match(event.request)
+  //     .then(function(response) {
+  //       if (response) {
+  //         return response;
+  //       } else {
+  //         return fetch(event.request)
+  //           .then(function(res) {
+  //             return caches.open(CACHE_DYNAMIC_NAME)
+  //               .then(function(cache) {
+  //                 cache.put(event.request.url, res.clone());
+  //                 return res;
+  //               })
+  //           })
+  //           .catch(function(err) {
+  //
+  //           });
+  //       }
+  //     })
+  // );
 });
 
 self.addEventListener('activate', function (event) {
